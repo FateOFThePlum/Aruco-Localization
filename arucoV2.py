f1105871ce8a645514ac1839f5c09e0a2f90874c
Logan Delplanche <109813637+FateOFThePlum@users.noreply.github.com>
2024-06-25 22:14:51 -0700

    Got arucoV2.py running on jetson with imx219
    I got rid of waitkey() and imshow() because of how opencv compiled, but I dont think that we need imshow() and waitkey() is to just give us time to look at the image.

--- arucoV2.py
+++ arucoV2.py
@@ -2,7 +2,6 @@ import cv2
 import numpy as np
 import math
 
-cam = cv2.VideoCapture(0)
 
 dictionary = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_APRILTAG_36H11)
 parameters =  cv2.aruco.DetectorParameters()
@@ -24,6 +23,37 @@ objectPoints = np.array([
     [-markerSize/2,  markerSize/2, 0],  # Top left
 ], dtype=np.float32)
 
+
+
+def gstreamer_pipeline(
+    sensor_id=0,
+    capture_width=1920,
+    capture_height=1080,
+    display_width=960,
+    display_height=540,
+    framerate=30,
+    flip_method=0,
+):
+    return (
+        "nvarguscamerasrc sensor-id=%d ! "
+        "video/x-raw(memory:NVMM), width=(int)%d, height=(int)%d, framerate=(fraction)%d/1 ! "
+        "nvvidconv flip-method=%d ! "
+        "video/x-raw, width=(int)%d, height=(int)%d, format=(string)BGRx ! "
+        "videoconvert ! "
+        "video/x-raw, format=(string)BGR ! appsink"
+        % (
+            sensor_id,
+            capture_width,
+            capture_height,
+            framerate,
+            flip_method,
+            display_width,
+            display_height,
+        )
+    )
+
+
+cam = cv2.VideoCapture(gstreamer_pipeline(flip_method=2), cv2.CAP_GSTREAMER) #Needs to be adjusted based on the camera being used
 
 def takePicture(): #In a function to allow for better use on the Jetson
     ret, frame = cam.read()
@@ -51,9 +81,9 @@ while True:
         print(detectedMarkers[0])
     exitFrame = cv2.aruco.drawDetectedMarkers(frame, markerCorners, markerIds, (255, 255, 0))
 
-    cv2.imshow('Frame',frame)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+    #cv2.imshow('Frame',frame)
+    #if cv2.waitKey(1) & 0xFF == ord('q'):
+    # break
 
 cam.release()
 cv2.destroyAllWindows()
